0cec02e2531c924b7a8099590a3add159aa38325
Sevara Isroilova <sevara.i.d@gmail.com>
2020-11-10 12:48:00 -0500

    initial changes Its 12:10PM Its my third commit Its my first commit after creating Hello Class Third trial

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("This is my second practice ");
 //Mac users are best :)
+        //adding changes
     }
 }
