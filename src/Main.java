c48dc96b0286e8e3183ae9ceb69e50a0388da548
Sevara Isroilova <sevara.i.d@gmail.com>
2020-11-10 11:46:37 -0500

    hey

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Weather is sunny");
+    }
+}
